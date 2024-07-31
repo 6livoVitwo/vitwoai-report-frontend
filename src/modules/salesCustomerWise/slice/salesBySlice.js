9d401dc6b794360f6944bba1041eb4362aaced7b
Srijan Das <srijan@vitwo.in>
2024-07-31 12:39:04 +0530

    sales slice modified

--- src/modules/salesCustomerWise/slice/salesBySlice.js
+++ src/modules/salesCustomerWise/slice/salesBySlice.js
@@ -1,31 +1,31 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const salesApi = createApi({
-  reducerPath: "salesApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.0.133:8081/",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("authToken");
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
-  endpoints: (builder) => ({
-    fetchSales: builder.query({
-      query: ({ page, filters }) => {
-        return {
-          url: `sales/sales-groupby-data?pageSize=${page}`,
-          method: "POST",
-          body: JSON.stringify(filters),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-      },
-    }),
-  }),
+	reducerPath: 'salesApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://192.168.0.133:8081/',
+		prepareHeaders: (headers) => {
+			const token = localStorage.getItem('authToken');
+			if (token) {
+				headers.set('Authorization', `Bearer ${token}`);
+			}
+			return headers;
+		},
+	}),
+	endpoints: (builder) => ({
+		fetchSales: builder.query({
+			query: ({ page, filters }) => {
+				return {
+					url: `sales/sales-groupby-data?pageSize=${page}`,
+					method: 'POST',
+					body: JSON.stringify(filters),
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				};
+			},
+		}),
+	}),
 });
 
 export const { useFetchSalesQuery } = salesApi;
